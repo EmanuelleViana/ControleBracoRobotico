a2475cf1f0b03032bd24e71945dea1b54db4c705
izabe <izabe@LAPTOP-R7OSPI5V>
2018-09-23 15:14:41 -0300

    atualizacao

---
+++ src/pages/control/control.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ControlPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+
+@Component({
+  selector: 'page-control',
+  templateUrl: 'control.html',
+})
+export class ControlPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ControlPage');
+  }
+
+}
